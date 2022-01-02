59cd9b80e2003a5f638902478f1dd3d546d622c9
Emrah Akçal <48285856+EmrahAKCAL@users.noreply.github.com>
2022-01-02 18:45:17 +0200

    reference-types

---
+++ Reference-Types/reference-types.js
@@ -0,0 +1,14 @@
+let names=['Ali', 'Hasan', 'Can'];
+console.log(typeof names);  //oject
+
+let address={
+    city:'Ankara',
+    Country:'Turkey'
+};
+console.log(typeof address); //object
+
+let calculateAge= function(){
+    return 0;
+};
+console.log(typeof calculateAge); //function
+
