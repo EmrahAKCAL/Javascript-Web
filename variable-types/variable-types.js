c27873828a0c28730753848ab893a8299635b270
Emrah Akçal <48285856+EmrahAKCAL@users.noreply.github.com>
2022-01-02 10:59:33 +0200

    veriable-types

---
+++ variable-types/variable-types.js
@@ -0,0 +1,15 @@
+let firstName='Emrah';
+console.log(typeof firstName); /* string*/
+/* Bir karakter dizisi olduğu anlamına gelmektedir.*/
+
+let money= 250.5;
+console.log(typeof money); /*number*/
+/* Sayısal bir değişken olduğunu tanımlar*/
+
+let isActive= false;
+console.log(typeof isActive); /* boolean*/
+/*Herhangi bir koşul durumlarında kullanılabilir. */
+
+let job= null;
+console.log(typeof job); /*object*/
+/*job değişkenin içerisinde herhangi bir değerin atanmadığını tanımlar. */
