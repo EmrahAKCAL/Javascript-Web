eb34850302027f7c6da364febe381e3adbb4c42b
Emrah Akçal <48285856+EmrahAKCAL@users.noreply.github.com>
2022-01-03 20:53:19 +0200

    Dates-application

---
+++ Application-2(Dates)/tarih-saat.js
@@ -0,0 +1,3 @@
+console.log(new Date('8/24/2022')); //Sadece tarih bilgisini string tipinde gösterir.
+console.log(new Date(2010, 2, 24, 20, 59, 49 )); //burada sırasıyla yıl, ay, gün, saat, dakika ve saniye girilmiştir. Bu sıralama önemlidir.
+
