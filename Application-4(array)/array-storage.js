b186071f9a3201fd7068f272aa68e1a42cc6b2e4
Emrah Akçal <48285856+EmrahAKCAL@users.noreply.github.com>
2022-01-05 22:00:08 +0200

    array-storage

---
+++ Application-4(array)/array-storage.js
@@ -0,0 +1,11 @@
+var studentA=['Ali', 'Çelik', 2005];
+var studentB=['Zeynep', 'Arslan', 1998];
+var studentC=['Can', 'Polat', 2013];
+
+var students=[studentA, studentB, studentC]; //dizi içerisine dizi atama
+console.log(students);
+console.log(students[2]); //dizi içerisinde indekse karşılık gelen elemanı bulma
+console.log(students[2][1]); 
+
+
+
