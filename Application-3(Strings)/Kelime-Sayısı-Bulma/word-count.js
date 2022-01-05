07acbe568dd6c972e0098bd7dcd4cdbca4842efe
Emrah Akçal <48285856+EmrahAKCAL@users.noreply.github.com>
2022-01-05 09:39:43 +0200

    word-count

---
+++ Application-3(Strings)/Kelime-Sayısı-Bulma/word-count.js
@@ -0,0 +1,4 @@
+//Bir metin içerisindeki kelime sayısı
+var text="Geceler boyunca süren güçlü gelgitlerden sonra, sayıları pek çok olan o hızlı gemiler hep birden enginlere doğru yelken açtılar ve hep akıntıya karşı yarıştılar. İçlerinden sadece bir tanesi o karanlık mağaradaki limana ulaşabildi. Orada demirlemiş olarak bekleyen öteki geminin içine yükünü boşalttı ve böylece yeni bir öykü başladı."
+
+console.log(text.trim().split(' ').length);
