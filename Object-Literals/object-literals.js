a4b40fe3a7f7275d5c595519fc70fe41a4801ddb
Emrah Akçal <48285856+EmrahAKCAL@users.noreply.github.com>
2022-01-07 18:54:24 +0200

    object literals

---
+++ Object-Literals/object-literals.js
@@ -0,0 +1,31 @@
+//bir obje kullanımı
+let admin={
+    username: 'Emrah',
+    password: 12345
+}
+console.log(admin);
+
+//object içerisinde object kullanımı
+let Admin={
+    firstName: 'Fatih',
+    lastName: 'Arslan',
+    address:{
+        city: 'Ankara',
+        country:'Turkey'
+    }
+}
+console.log(Admin);
+
+
+//object içerisinde dizi ve object kullanımı
+let person={
+    firstName: 'Emrah',
+    lastName: 'Akçal',
+    age: 26,
+    hobbies: ['Music', 'Football', 'Walk', 'Cinema'],
+    address: {
+        city: 'Adıyaman',
+        country: 'Turkey'
+    }
+}
+console.log(person);
