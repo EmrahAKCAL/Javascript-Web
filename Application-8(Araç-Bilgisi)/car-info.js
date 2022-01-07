685a66f1d3d61a695d36aaf19f76b27d35272684
Emrah Akçal <48285856+EmrahAKCAL@users.noreply.github.com>
2022-01-07 21:29:56 +0200

    car-informations

---
+++ Application-8(Araç-Bilgisi)/car-info.js
@@ -0,0 +1,169 @@
+var CarInfo={
+    Id: 'BMW116d_123',
+    Model: 'BMW 116d',
+    Year: 2015,
+    Color: 'Beyaz',
+    ServiceRecords:[
+        {
+            Id: 'BMW116d_123_1',
+            Dt: '17.05.2016',
+            Km: '13200',
+            TotalF: 1200,
+            Details:[
+                {
+                    Id: 'BMW116d_123_1_1',
+                    Statemen: 'Yağ değişimi',
+                    Fee: 400,
+                },
+                {
+                    Id: 'BMW116d_123_1_2',
+                    Statemen: 'Filtre değişimi',
+                    Fee: 350,
+                },
+                {
+                    Id: 'BMW116d_123_1_3',
+                    Statemen: 'Fren hidroliği değişimi',
+                    Fee: 450,
+                }
+            ]
+        },
+        {
+            Id: 'BMW116d_123_2',
+            Dt: '21.03.2017',
+            Km: '16800',
+            TotalF: 1750,
+            Details:[
+                {
+                    Id: 'BMW116d_123_2_1',
+                    Statemen: 'Yağ değişimi',
+                    Fee: 500
+                },
+                {
+                    Id: 'BMW116d_123_2_2',
+                    Statemen: 'Filtre değişimi',
+                    Fee: 350
+                },
+                {
+                    Id: 'BMW116d_123_2_3',
+                    Statemen: 'Balata değişimi',
+                    Fee: 550
+                },
+                {
+                    Id: 'BMW116d_123_2_4',
+                    Statemen: 'Fren hidroliği değişimi',
+                    Fee: 350
+                }
+
+            ]
+        },
+        {
+            Id: 'BMW116d_123_3',
+            Dt: '05.01.2018',
+            Km: '20000',
+            TotalF: 850,
+            Details:[
+                {
+                    Id: 'BMW116d_123_3_1',
+                    Statemen: 'Yağ değişimi',
+                    Fee: 300
+                },
+                {
+                    Id: 'BMW116d_123_3_2',
+                    Statemen: 'Filtre değişimi',
+                    Fee: 350
+                },
+                {
+                    Id: 'BMW116d_123_3_3',
+                    Statemen: 'Cam silecek değişimi',
+                    Fee: 200
+                }
+
+            ]
+        },
+        {
+            Id: 'BMW116d_123_4',
+            Dt: '18.06.2019',
+            Km: '24000',
+            TotalF: 1750,
+            Details:[
+                {
+                    Id: 'BMW116d_123_4_1',
+                    Statemen: 'Yağ değişimi',
+                    Fee: 300
+                },
+                {
+                    Id: 'BMW116d_123_4_2',
+                    Statemen: 'Filtre değişimi',
+                    Fee: 450
+                },
+                {
+                    Id: 'BMW116d_123_4_3',
+                    Statemen: 'Balata değişimi',
+                    Fee: 480
+                },
+                {
+                    Id: 'BMW116d_123_4_4',
+                    Statemen: 'Fren hidroliği değişimi',
+                    Fee: 520
+                }
+
+            ]
+        },
+        {
+            Id: 'BMW116d_123_5',
+            Dt: '25.02.2020',
+            Km: '27000',
+            TotalF: 1450,
+            Details:[
+                {
+                    Id: 'BMW116d_123_5_1',
+                    Statemen: 'Yağ değişimi',
+                    Fee: 300
+                },
+                {
+                    Id: 'BMW116d_123_5_2',
+                    Statemen: 'Filtre değişimi',
+                    Fee: 520
+                },
+                {
+                    Id: 'BMW116d_123_5_3',
+                    Statemen: 'Fren hidroliği değişimi',
+                    Fee: 630
+                }
+
+            ]
+        },
+        {
+            Id: 'BMW116d_123_6',
+            Dt: '08.11.2021',
+            Km: '30000',
+            TotalF: 1650,
+            Details:[
+                {
+                    Id: 'BMW116d_123_2_1',
+                    Statemen: 'Yağ değişimi',
+                    Fee: 550
+                },
+                {
+                    Id: 'BMW116d_123_2_2',
+                    Statemen: 'Filtre değişimi',
+                    Fee: 250
+                },
+                {
+                    Id: 'BMW116d_123_2_3',
+                    Statemen: 'Balata değişimi',
+                    Fee: 500
+                },
+                {
+                    Id: 'BMW116d_123_2_4',
+                    Statemen: 'Fren hidroliği değişimi',
+                    Fee: 350
+                }
+
+            ]
+        },
+    ]
+
+}
+
+console.log(CarInfo);
